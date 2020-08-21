a01ba7271b0d74141eb3f3de1ba71fc38d9f1767
Mauro Dorigo <mau.dorigo@outlook.es>
2020-08-21 20:31:31 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,9 +1,9 @@
 def square_array(array)
+  new_array =[]
   counter = 0
-  
-    while counter < array.length do
-    array[counter] ** 2
+  while counter < array.length do
+    new_array.push(array[counter] ** 2)
     counter += 1
-  end
-  
+    end
+  new_array
 end
